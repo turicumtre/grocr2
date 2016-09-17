51546c422c7736beacefe35991ac7ee61218329e
Neli2 <cornelia.scherrer@teemo.de>
2016-09-17 19:54:01 +0200

    activity

--- app/src/main/java/com/example/patrick/grocr2/ProductBrowser.java
+++ app/src/main/java/com/example/patrick/grocr2/ProductBrowser.java
@@ -55,15 +55,6 @@ public class ProductBrowser extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         button = (Button) findViewById(R.id.searchb);
 
         button.setOnClickListener(new View.OnClickListener() {
