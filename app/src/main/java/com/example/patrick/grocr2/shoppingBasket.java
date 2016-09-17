c8dd25b995313af830fd12f1f934645cb0c5ef11
Neli2 <cornelia.scherrer@teemo.de>
2016-09-17 20:23:25 +0200

    activity

--- app/src/main/java/com/example/patrick/grocr2/shoppingBasket.java
+++ app/src/main/java/com/example/patrick/grocr2/shoppingBasket.java
@@ -24,20 +24,6 @@ public class shoppingBasket extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_basket);
 
-        //toolbar with back button
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(R.string.title_shopping_basket);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
 
         TextView numOfItems = (TextView) findViewById(R.id.numOfItems);
         TextView totalPrice = (TextView) findViewById(R.id.totalPrice);
