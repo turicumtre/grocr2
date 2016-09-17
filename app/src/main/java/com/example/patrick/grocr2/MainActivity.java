a5c5722909df87f785a222d4253efbd609fd8325
Neli2 <cornelia.scherrer@teemo.de>
2016-09-17 02:50:44 +0200

    init login

--- app/src/main/java/com/example/patrick/grocr2/MainActivity.java
+++ app/src/main/java/com/example/patrick/grocr2/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.patrick.grocr2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button logoutButton = (Button) findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, login.class);
+                startActivity(myIntent);
+            }
+        });
+
     }
 }
